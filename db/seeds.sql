f523b2b93e66e96d4de5458c43f1dafca619535a
Sushi Sama <Phongsavanhx@gmail.com>
2019-01-27 02:17:30 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Seafood Burger', true);
