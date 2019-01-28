57510172efe4f1621097d53f643beda20c282fcd
Phongsavanh Xayvongsack <phongsavanhx@gmail.com>
2019-01-27 21:36:35 -0700

    asdfa

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,10 @@ var mysql = require('mysql');
 // Create the MySQL connection object
 var connection;
 
-if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	// DB is local on localhost
-	connection = mysql.createConnection({
-		port: 3306,
-		host: 'localhost',
-		user: 'root',
+connection = mysql.createConnection({
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
         password: 'april1690',
 		database: 'burgers_db'
 	})
